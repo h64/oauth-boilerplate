6f941f89230012eb44ca9311069e9ad2b08876fd
h64 <hhongnr@gmail.com>
2019-09-06 17:22:55 -0700

    Fixed incorrect headers

--- controllers/api.js
+++ controllers/api.js
@@ -7,16 +7,15 @@ router.get('/repos', (req, res) => {
     let config = {
         // NOTE - Set User-Agent to whatever your github's app name is 
         headers: {
-            'Authorization': `Bearer: ${req.user.accessToken}`,
+            Authorization: `token ${req.user.accessToken}`,
             'User-Agent': 'henry-oauth-boilerplate'
         }
     }
     // console.log(config);
-    axios.get('https://api.github.com/users/repos', config)
+    axios.get('https://api.github.com/user/repos', config)
         .then(response => {
-            // console.log(response.data);
-            res.json(response.data);
-            // res.render('repos', { repos: response.data })
+            // res.send(response.data);
+            res.render('repos', { repos: response.data })
         })
         .catch(err => {
             console.log(err);
